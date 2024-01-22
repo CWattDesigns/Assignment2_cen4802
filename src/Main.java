74bb283e984246ffc819ef7547011fd58f07163f
Christopher Wattles <87830172+CWattDesigns@users.noreply.github.com>
2024-01-21 19:44:29 -0600

    Added javadoc comments

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    /**
+     * CalculateFibonacci
+     * @param n
+     * @return n or result from calculation
+     * Description: Given the number is greater than 1, recursively call this function to calculate the value at
+     * the nth Fibonacci Sequence.
+     */
     public static int calculateFibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -7,6 +14,14 @@ public class Main {
         }
     }
 
+    /**
+     * Main
+     * @param args
+     * @return no return value
+     * Description: Provides the nth sequence number, then passes that to the calculateFibonacci function.
+     * It then outputs the result of that recursive function to tell the user what the value is at the nth Fibonacci
+     * Sequence.
+     */
     public static void main(String[] args) {
         int n = 10;
         int nthTerm = calculateFibonacci(n);
